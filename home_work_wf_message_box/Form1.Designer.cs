ebc20284da158b920be5ecb6901baf3cc4d00b32
Василь Колотюк <vasakolotuk02@gmail.com>
2020-10-25 12:40:35 +0200

    first commit

---
+++ home_work_wf_message_box/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace home_work_wf_message_box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(320, 165);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(166, 34);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(293, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "загадайте число від 1 до 100 і нажміть start";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        protected System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ home_work_wf_message_box/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace home_work_wf_message_box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            int number = 50;
+            bool exit = true;
+            int min = 1, max = 100;
+            int count = 0;
+            do
+            {
+                DialogResult res1 = MessageBox.Show($"це твоє число {number} ?", "game!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (res1 == DialogResult.Yes)
+                {
+                    DialogResult res2 = MessageBox.Show($"я вгадав твоє число {number}\nкількість спроб: {count}\nбажеєш продовжити гру", "game!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (res2 == DialogResult.Yes)
+                    {
+                        number = 50;
+                        count = 0;
+                        min = 1;
+                        max = 100;
+                    }
+                    if (res2 == DialogResult.No)
+                    {
+                        exit = false;
+                    }
+                }
+                else if (res1 == DialogResult.No)
+                {
+                    count++;
+                    if (min == max)
+                    { MessageBox.Show($"лгун ето твое число признайся -->{ number}", "game!", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+                    else
+                    {
+                        DialogResult res3 = MessageBox.Show($"Загаданное число меньше или равно {number}", "game!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (res3 == DialogResult.Yes)
+                        {
+                            max = number;
+
+                        }
+                        else if (res3 == DialogResult.No)
+                        {
+
+                            min = number + 1;
+
+                        }
+                        number = (min + max) / 2;
+                    }
+                }
+            } while (exit);
+        }
+
+    }
+}
